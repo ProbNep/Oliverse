beab2b4d7977d69ccc2198ffa4d40c1ae2d5b475
neppy <ryajabr99@gmail.com>
2026-06-05 23:45:06 +0300

    add plugins array to vite config for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // We leave this empty because Lovable's config package automatically
-  // handles the plugins under the hood without breaking the compiler.
+  vite: {
+    plugins: []
+  }
 });
